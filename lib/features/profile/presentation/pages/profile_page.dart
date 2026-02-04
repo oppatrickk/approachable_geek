acb53146b487d19028c52dfdbfee075fc6e89d09
John Patrick Prieto <prieto.patrick15@gmail.com>
2026-02-04 12:54:24 +0800

    feat(profile): update page and add refresh

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -1,10 +1,13 @@
 import 'package:approachable_geek/core/enums/profile_item_type.dart';
 import 'package:approachable_geek/core/utils/extensions.dart';
 import 'package:approachable_geek/core/utils/ui_helpers.dart';
+import 'package:approachable_geek/features/profile/domain/entities/profile.dart';
+import 'package:approachable_geek/features/profile/presentation/blocs/profile_bloc.dart';
 import 'package:approachable_geek/features/profile/presentation/widgets/profile_item.dart';
 import 'package:approachable_geek/features/profile/presentation/widgets/profile_photo.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 @RoutePage()
 class ProfilePage extends StatelessWidget {
@@ -21,46 +24,65 @@ class ProfilePage extends StatelessWidget {
         body: SafeArea(
           child: RawScrollbar(
             thumbColor: context.colorScheme.primary.withAlpha(100),
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    verticalSpace(24),
-                    Center(
-                      child: Text(
-                        'Edit Profile',
-                        style: context.textTheme.headlineMedium?.semibold.cColor(context.colorScheme.primary),
-                      ),
-                    ),
-                    verticalSpace(24),
-                    ProfilePhoto(),
-                    verticalSpace(24),
-                    ProfileItem(
-                      type: ProfileItemType.name,
-                      title: 'Name',
-                      content: 'Micah Smith',
-                    ),
-                    ProfileItem(
-                      type: ProfileItemType.phone,
-                      title: 'Phone',
-                      content: '(208) 206-5039',
-                    ),
-                    ProfileItem(
-                      type: ProfileItemType.email,
-                      title: 'Email',
-                      content: 'micahsmith@gmail.com',
-                    ),
-                    ProfileItem(
-                      type: ProfileItemType.about,
-                      title: 'Tell us about yourself',
-                      content:
-                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                    ),
-                    verticalSpace(24),
-                  ],
+            child: RefreshIndicator(
+              onRefresh: () async {
+                context.read<ProfileBloc>().add(const ProfileEvent.getUserProfile());
+              },
+              child: SingleChildScrollView(
+                physics: const AlwaysScrollableScrollPhysics(),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16),
+                  child: BlocBuilder<ProfileBloc, ProfileState>(
+                    builder: (context, state) {
+                      return state.maybeMap(
+                        initial: (_) => const SizedBox.shrink(),
+                        loading: (ProfileLoading value) => const Center(child: CircularProgressIndicator()),
+                        loaded: (ProfileLoaded value) {
+                          Profile user = value.profile;
+
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              verticalSpace(24),
+                              Center(
+                                child: Text(
+                                  'Edit Profile',
+                                  style: context.textTheme.headlineMedium?.semibold.cColor(context.colorScheme.primary),
+                                ),
+                              ),
+                              verticalSpace(24),
+                              ProfilePhoto(user: user),
+                              verticalSpace(24),
+                              ProfileItem(
+                                user: user,
+                                type: ProfileItemType.name,
+                                title: 'Name',
+                              ),
+                              ProfileItem(
+                                user: user,
+                                type: ProfileItemType.phone,
+                                title: 'Phone',
+                              ),
+                              ProfileItem(
+                                user: user,
+                                type: ProfileItemType.email,
+                                title: 'Email',
+                              ),
+                              ProfileItem(
+                                user: user,
+                                type: ProfileItemType.about,
+                                title: 'Tell us about yourself',
+                              ),
+                              verticalSpace(24),
+                            ],
+                          );
+                        },
+                        error: (ProfileError value) => Center(child: ErrorWidget(value.message)),
+                        orElse: () => const SizedBox.shrink(),
+                      );
+                    },
+                  ),
                 ),
               ),
             ),
