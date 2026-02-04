99838402b9d7884387a3628c9548a47f390eb9aa
John Patrick Prieto <prieto.patrick15@gmail.com>
2026-02-04 12:56:18 +0800

    feat(profile): update about section

--- lib/features/profile/presentation/components/profile_update_about.dart
+++ lib/features/profile/presentation/components/profile_update_about.dart
@@ -1,9 +1,19 @@
 import 'package:approachable_geek/core/ui/custom_button.dart';
+import 'package:approachable_geek/core/ui/custom_snackbar.dart';
 import 'package:approachable_geek/core/ui/custom_text_field.dart';
+import 'package:approachable_geek/features/profile/domain/entities/profile.dart';
+import 'package:approachable_geek/features/profile/presentation/blocs/profile_bloc.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProfileUpdateAboutSection extends StatefulWidget {
-  const ProfileUpdateAboutSection({super.key});
+  const ProfileUpdateAboutSection({
+    super.key,
+    required this.user,
+  });
+
+  final Profile user;
 
   @override
   State<ProfileUpdateAboutSection> createState() => _ProfileUpdateAboutSectionState();
@@ -13,13 +23,61 @@ class _ProfileUpdateAboutSectionState extends State<ProfileUpdateAboutSection> {
   TextEditingController aboutController = TextEditingController();
   FocusNode aboutFocusNode = FocusNode();
 
+  String? initialAbout;
+
   @override
   void initState() {
     super.initState();
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
+      aboutController.text = widget.user.about;
+      initialAbout = widget.user.about;
+
+      aboutController.addListener(onAboutChanged);
+
       aboutFocusNode.requestFocus();
     });
+  }
+
+  void onAboutChanged() {
+    setState(() {});
+  }
+
+  bool get _isChanged {
+    final aboutChanged = aboutController.text.trim() != initialAbout;
+    final isNotEmpty = aboutController.text.trim().isNotEmpty;
+    return aboutChanged && isNotEmpty;
+  }
+
+  @override
+  void dispose() {
+    aboutController.removeListener(onAboutChanged);
+    aboutController.dispose();
+    aboutFocusNode.dispose();
+    super.dispose();
+  }
+
+  void onSubmit() {
+    context.read<ProfileBloc>().add(
+      ProfileEvent.updateUserProfile(
+        Profile(
+          uid: widget.user.uid,
+          firstName: widget.user.firstName,
+          lastName: widget.user.lastName,
+          email: widget.user.email,
+          phone: widget.user.phone,
+          about: aboutController.text.trim(),
+          photo: widget.user.photo,
+        ),
+      ),
+    );
+
+    showCustomSnackbar(
+      context: context,
+      message: 'About updated successfully',
+    );
+
+    context.router.pop();
   }
 
   @override
@@ -36,7 +94,7 @@ class _ProfileUpdateAboutSectionState extends State<ProfileUpdateAboutSection> {
                   hint: 'Write a little bit about yourself. Do you like chatting? Are you a smoker? Do you bring pets with you? Etc.',
                   minLines: 5,
                   maxLines: 5,
-                  maxLength: 500,
+                  maxLength: 200,
                   keyboardType: TextInputType.multiline,
                   textInputAction: TextInputAction.newline,
                 ),
@@ -45,8 +103,9 @@ class _ProfileUpdateAboutSectionState extends State<ProfileUpdateAboutSection> {
           ),
           Spacer(),
           CustomButton(
+            enabled: _isChanged,
             label: 'Update',
-            onPressed: () {},
+            onPressed: () => onSubmit(),
           ),
         ],
       ),
--- lib/features/profile/presentation/pages/profile_update_page.dart
+++ lib/features/profile/presentation/pages/profile_update_page.dart
@@ -1,6 +1,7 @@
 import 'package:approachable_geek/core/enums/profile_item_type.dart';
 import 'package:approachable_geek/core/utils/extensions.dart';
 import 'package:approachable_geek/core/utils/ui_helpers.dart';
+import 'package:approachable_geek/features/profile/domain/entities/profile.dart';
 import 'package:approachable_geek/features/profile/presentation/components/profile_update_about.dart';
 import 'package:approachable_geek/features/profile/presentation/components/profile_update_email.dart';
 import 'package:approachable_geek/features/profile/presentation/components/profile_update_name.dart';
@@ -15,9 +16,11 @@ class ProfileUpdatePage extends StatelessWidget {
   const ProfileUpdatePage({
     super.key,
     required this.profileItemType,
+    required this.user,
   });
 
   final ProfileItemType profileItemType;
+  final Profile user;
 
   @override
   Widget build(BuildContext context) {
@@ -60,15 +63,15 @@ class ProfileUpdatePage extends StatelessWidget {
   Widget buildSections(ProfileItemType profileItemType) {
     switch (profileItemType) {
       case ProfileItemType.name:
-        return ProfileUpdateNameSection();
+        return ProfileUpdateNameSection(user: user);
       case ProfileItemType.phone:
-        return ProfileUpdatePhoneSection();
+        return ProfileUpdatePhoneSection(user: user);
       case ProfileItemType.email:
-        return ProfileUpdateEmailSection();
+        return ProfileUpdateEmailSection(user: user);
       case ProfileItemType.about:
-        return ProfileUpdateAboutSection();
+        return ProfileUpdateAboutSection(user: user);
       case ProfileItemType.photo:
-        return ProfileUpdatePhotoSection();
+        return ProfileUpdatePhotoSection(user: user);
     }
   }
 }
